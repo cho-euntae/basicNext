9c69c14124f9bcdfcc1b0f8857bc869f279afa2d
euntae.cho <darksyains@gmail.com>
2024-06-11 23:56:33 +0900

    poster add

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 ## git add .
 ## git commit -m 'test'
 ## git push -u origin main
+
+
+##### ERROR LIST
+## ------------------------------------ 1
+# warning: in the working copy of 'app/layout.tsx', LF will be replaced by CRLF the next time Git touches it
+
+## Window
+### git config --global core.autocrlf true
+
+## Mac
+### git config --global core.autocrlf input
+## ------------------------------------ 1
+
+
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,14 +1,15 @@
 'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation(){
 
     const path = usePathname();
     console.log("console Navigation path = "+path);
     return (
-    <nav>
-        <ul>
+    <nav className={styles.nav}>
+        <ul className={styles.list}>
             <li>
                 <Link href="/">Home</Link> {path === "/" ? "★" : ""}
             </li>
